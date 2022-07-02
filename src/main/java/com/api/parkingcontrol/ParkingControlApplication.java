2147313004687c90a34dc1ccd7f54177c1347297
João Victor Soares Gomes <joaovsg@id.uff.br>
2022-07-02 02:24:11 -0300

    Configuring Swagger to do the documentation of the API and pulling out the hello World in the / Get mapping.

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -12,10 +12,5 @@ public class ParkingControlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
 	}
-
-	@GetMapping("/")
-	public String index(){
-		return "Olá Mundo!";
-	}
-
+	
 }
